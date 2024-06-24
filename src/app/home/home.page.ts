247e094a16595907406461084e5b137539942e36
CucuHanapiyani <cucuhanapiyanii@gmail.com>
2024-06-24 20:09:12 +0700

    update home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  books: any[] = [];
 
-  constructor() {}
+  constructor(private apiService: ApiService) { }
 
+  ngOnInit() {
+    this.apiService.getBooks().subscribe((data: any[]) => {
+      this.books = data;
+    });
+  }
 }
--- src/app/library/library.page.ts
+++ src/app/library/library.page.ts
@@ -8,25 +8,35 @@ import { Router } from '@angular/router';
 })
 export class LibraryPage {
   books: any[] = [
-    { id: 1, title: 'Laut Bercerita', author: 'haris', imageUrl: 'assets/imgs/1.jpg' },
+    { id: 1, title: 'Laut Bercerita', author: 'Leila S. Chudori', imageUrl: 'assets/imgs/1.jpg' },
+    { id: 2, title: 'Hana Bocil', author: 'Pramoedya Ananta Toer', imageUrl: 'assets/imgs/2.jpg' },
+    { id: 3, title: 'Danto Bercerita', author: 'Andrea Hirata', imageUrl: 'assets/imgs/3.jpg' },
+    { id: 4, title: 'Amel Bucin', author: 'Author Name', imageUrl: 'assets/imgs/4.jpg' },
+    { id: 5, title: 'Ikbal Sad Boy', author: 'Author Name', imageUrl: 'assets/imgs/5.jpg' },
+    { id: 6, title: 'Zainal Bad Boy', author: 'Author Name', imageUrl: 'assets/imgs/6.jpg' },
     // Tambahkan buku lainnya sesuai kebutuhan
   ];
 
-  filteredBooks: any[] = [];
+  filteredBooks: any[] = []; // Awalnya kosong
+  searchTerm: string = ''; // Untuk menyimpan kata kunci pencarian
 
-  constructor(private router: Router) {
-    this.filteredBooks = [...this.books]; // Menyalin semua buku ke hasil pencarian awal
-  }
+  constructor(private router: Router) {}
 
   onSearch(event: any) {
-    const searchTerm = event.target.value.toLowerCase();
+    this.searchTerm = event.target.value.toLowerCase();
 
-    if (searchTerm.trim() === '') {
-      this.filteredBooks = [...this.books]; // Kembali ke daftar lengkap jika kotak pencarian kosong
+    // Filter buku berdasarkan input pencarian
+    if (this.searchTerm.trim() === '') {
+      this.filteredBooks = []; // Kosongkan jika kotak pencarian kosong
     } else {
       this.filteredBooks = this.books.filter(book =>
-        book.title.toLowerCase().includes(searchTerm) || book.author.toLowerCase().includes(searchTerm)
+        book.title.toLowerCase().includes(this.searchTerm) || book.author.toLowerCase().includes(this.searchTerm)
       );
     }
+  }
+
+  openBook(bookId: number) {
+    // Navigasi ke halaman detail buku berdasarkan ID
+    this.router.navigate(['/book-detail', bookId]);
   }
 }
